3ddd2124f750a8b4996e19731b70fa5644f34550
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-04-07 02:38:50 -0700

    Bind to 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import app from './app';
 
 const port = Number(process.env.MEAL_LOG_PORT) || 3006;
 
-app.listen({port});
-console.log(`Meal Log server running on 127.0.0.1:${port}`);
+app.listen({host: '0.0.0.0', port});
